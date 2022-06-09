68ea092b0305d4054c96c5a379b7f52332646a85
Piotr Kowalski <piecioshka@gmail.com>
2022-06-09 21:33:45 +0200

    Update deps

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,7 +1,3 @@
 export class UserModel {
-  constructor(
-    public email: string,
-    public password: string,
-    public sex?: "m" | "f"
-  ) {}
+  constructor(public email: string, public password: string) {}
 }
